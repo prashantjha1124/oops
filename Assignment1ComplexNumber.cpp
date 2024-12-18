939fb2b38b51dbb317816de3436d14c5a8b80692
prashantjha1124 <166237885+prashantjha1124@users.noreply.github.com>
2024-12-18 10:10:11 +0530

    Assignment1ComplexNumber.cpp

--- Assignment1ComplexNumber.cpp
+++ Assignment1ComplexNumber.cpp
@@ -1,47 +1,65 @@
-#include <iostream> 
+#include <iostream>
 using namespace std;
-class Complex
-{
+
+class Complex {
 public:
-int real; int imaginary;
-Complex()
-{
-real=0; 	
-imaginary=0;
-cout<<"Default constructor value:"<<real<<" + "<<imaginary<<"i"<<endl;
-}
-Complex operator+(Complex b)
-{
-Complex temp;
-temp.real=real+b.real; temp.imaginary=imaginary+b.imaginary;
-cout<<"Addition is:"<<temp.real<<" + "<<temp.imaginary<<"i"<<endl;
-}
-Complex operator*(Complex b)
-{
-Complex temp;
-temp.real=(real*b.real)-(imaginary*b.imaginary); temp.imaginary=(real*b.imaginary)+(imaginary*b.real);
-cout<<"Multiplication is:"<<temp.real<<" + "<<temp.imaginary<<"i"<<endl; return temp;
-}
-friend ostream &operator<<(ostream &output, Complex &m);
- friend istream &operator<<(istream &intput, Complex &m);
+    int real;
+    int imaginary;
+
+    // Default constructor
+    Complex() : real(0), imaginary(0) {
+        cout << "Default constructor value: " << real << " + " << imaginary << "i" << endl;
+    }
+
+    // Parameterized constructor
+    Complex(int r, int i) : real(r), imaginary(i) {}
+
+    // Overloading the + operator
+    Complex operator+(const Complex& b) const {
+        return Complex(real + b.real, imaginary + b.imaginary);
+    }
+
+    // Overloading the * operator
+    Complex operator*(const Complex& b) const {
+        return Complex((real * b.real) - (imaginary * b.imaginary), (real * b.imaginary) + (imaginary * b.real));
+    }
+
+    // Friend function for overloading << operator
+    friend ostream& operator<<(ostream& output, const Complex& m);
+
+    // Friend function for overloading >> operator
+    friend istream& operator>>(istream& input, Complex& m);
 };
-ostream &operator<<( ostream &output, Complex &m ) 
-{ output <<"\n"<<m.real<<" + "<<m.imaginary<<"i"<<endl; 
-return output;
+
+// Overloading the << operator
+ostream& operator<<(ostream& output, const Complex& m) {
+    output << m.real << " + " << m.imaginary << "i";
+    return output;
 }
-istream &operator>>( istream &input, Complex &m )
-{
-input >> m.real>>m.imaginary;
-return input;
+
+// Overloading the >> operator
+istream& operator>>(istream& input, Complex& m) {
+    input >> m.real >> m.imaginary;
+    return input;
 }
-int main()
-{
-Complex a;
-cout<<"\n Enter first complex number:"<<endl;
-cin>>a;
-Complex b;
-cout<<"\n Enter second complex number:"<<endl; cin>>b;
-cout<<"\n first complex number:"<<endl; cout<<a;
-cout<<"\n second complex number:"<<endl;
-cout<<b; Complex c=a+b; Complex d=a*b; return 0;
+
+int main() {
+    Complex a;
+    cout << "\nEnter first complex number (real and imaginary parts): ";
+    cin >> a;
+
+    Complex b;
+    cout << "\nEnter second complex number (real and imaginary parts): ";
+    cin >> b;
+
+    cout << "\nFirst complex number: " << a << endl;
+    cout << "\nSecond complex number: " << b << endl;
+
+    Complex c = a + b;
+    cout << "\nAddition result: " << c << endl;
+
+    Complex d = a * b;
+    cout << "\nMultiplication result: " << d << endl;
+
+    return 0;
 }
